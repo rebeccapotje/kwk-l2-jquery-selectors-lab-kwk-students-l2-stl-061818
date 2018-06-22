4aa8e34977fe437c1abaf3eedee13ca60e091453
rebeccapotje <rebecca.potje@gmail.com>
2018-06-22 18:48:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,24 @@
 'use-strict';
 //add your code here
+
+function h1Selector (element) {
+  document.querySelectorAll('h1');
+  return document.querySelectorAll('h1') 
+}
+
+function liInOlSelector (element) {
+  document.querySelectorAll('ol li');
+  return document.querySelectorAll('ol li')
+}
+
+function linkSelector (element) {
+  return $('#box4.box5 a')
+}
+
+function imageSelector (element) {
+  return $("[alt = 'cat sleeping']")
+}
+
+function checkboxInputSelector (element) {
+  return $(":checkbox")
+}
